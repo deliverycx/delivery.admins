89f36752c5dfcf175871cd7568ec28de6ebc92e7
xirurgx3d <xirurgx3d@gmail.com>
2022-04-28 16:51:53 +0300

    Update local.strategy.ts

--- backend/src/components/common/auth/strategy/local.strategy.ts
+++ backend/src/components/common/auth/strategy/local.strategy.ts
@@ -11,7 +11,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   async validate(username: string, password: string): Promise<any> {
     const user = await this.LoginServises.validateUsers(username, password);
-    console.log(user);
+    console.log('user',user);
     if (!user) {
       throw new UnauthorizedException()
     }
