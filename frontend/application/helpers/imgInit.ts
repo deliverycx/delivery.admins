1c196f39bf1051d5cd4b20bce684cd11e38eead4
xirurgx3d <xirurgx3d@gmail.com>
2022-06-07 17:05:32 +0300

    bag img

--- frontend/application/helpers/imgInit.ts
+++ frontend/application/helpers/imgInit.ts
@@ -2,5 +2,5 @@
 export const imgRout = (img:string) => {
   return (process.env.NODE_ENV === 'development')
     ? `${process.env.NEXT_PUBLIC_STATIC}/static/${img}`
-    : img
+    : `/static/${img}`
 }
