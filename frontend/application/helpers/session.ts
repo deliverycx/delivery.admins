e5a8e7c04d8a00b9cbd86c0c408a54a6c11bdffe
xirurgx3d <xirurgx3d@gmail.com>
2023-02-28 14:24:13 +0300

    Update session.ts

--- frontend/application/helpers/session.ts
+++ frontend/application/helpers/session.ts
@@ -3,11 +3,11 @@ import type { IronSessionOptions } from 'iron-session'
 import { withIronSessionSsr } from 'iron-session/next'
 
 export const sessionOptions: IronSessionOptions = {
-  password: process.env.SECRET_COOKIE_PASSWORD as string,
+  password: '123', //process.env.SECRET_COOKIE_PASSWORD as string,
   cookieName: 'iron-session/examples/next.js',
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
-    secure:process.env.NODE_ENV === 'production',
+    secure: false //process.env.NODE_ENV === 'production',
   },
 }
 
