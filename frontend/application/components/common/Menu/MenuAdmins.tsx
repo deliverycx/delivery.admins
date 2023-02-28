0727ff92fc7a422e9bb07ad466a6a14ecbfb7aa7
xirurgx3d <xirurgx3d@gmail.com>
2023-02-28 17:23:59 +0300

    Update MenuAdmins.tsx

--- frontend/application/components/common/Menu/MenuAdmins.tsx
+++ frontend/application/components/common/Menu/MenuAdmins.tsx
@@ -11,7 +11,7 @@ const MenuAdmins = () =>{
         <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           
           <li className="nav-item">
-            <a href="/admins" className="nav-link">
+            <a href="/managers" className="nav-link">
               <i className="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Заказы оплата
