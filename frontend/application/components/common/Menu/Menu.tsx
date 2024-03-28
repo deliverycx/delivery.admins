f378f7467c8158bbf189ebbd4348a2a26a27eae7
xirurgx3d <xirurgx3d@gmail.com>
2024-03-28 15:05:14 +0300

    меню пользователей

--- frontend/application/components/common/Menu/Menu.tsx
+++ frontend/application/components/common/Menu/Menu.tsx
@@ -1,156 +1,140 @@
+import { ROUTE_PAGE } from "application/contstans/routs";
 import axios from "axios"
 import { userModel } from "domains/models/Users.model";
 import { observer } from "mobx-react-lite";
 import { useRouter } from 'next/router';
 
 
-const Menu = () =>{
+const Menu = () => {
 	const router = useRouter()
 
 	const user = userModel.user
-	//console.log('user',user?.pagesUser);
 
-	const deliteCookies = async () =>{
+
+	const deliteCookies = async () => {
 		try {
-			const {data} = await axios.get('/api/auth/logout')
-			if(data && data.ok){
+			const { data } = await axios.get('/api/auth/logout')
+			if (data && data.ok) {
 				router.push('/auth')
 			}
 		} catch (error) {
 			console.log(error);
 		}
-		
+
 	}
 
-  return (
-    <aside className="main-sidebar sidebar-dark-primary elevation-4">
-    
-    
-		
-    
-    <div className="sidebar">
-      
-      <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div className="image">
-          <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image"/>
-        </div>
-        <div className="info">
-          <a href="#" className="d-block">admin</a>
-					<a onClick={deliteCookies}>выйти</a>
-        </div>
-      </div>
+	return (
+		<aside className="main-sidebar sidebar-dark-primary elevation-4">
 
-      
-      <div className="form-inline">
-        <div className="input-group" data-widget="sidebar-search">
-          
-          <div className="input-group-append">
-            <button className="btn btn-sidebar">
-              <i className="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-      </div>
 
-     
-      <nav className="mt-2">
-        <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          
-          <li className="nav-item">
-						<a href="/dashbord/" className="nav-link">
-              <i className="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Дашборд
-                <i className="right fas fa-angle-left"></i>
-              </p>
+			{
+				user &&
+
+
+				<div className="sidebar">
+
+					<div className="user-panel mt-3 pb-3 mb-3 d-flex">
+						<div className="image">
+							<img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
+						</div>
+						<div className="info">
+							<a href="#" className="d-block">{user.name}</a>
+							<a onClick={deliteCookies}>выйти</a>
+						</div>
+					</div>
+
+
+					<div className="form-inline">
+						<div className="input-group" data-widget="sidebar-search">
+
+							<div className="input-group-append">
+								<button className="btn btn-sidebar">
+									<i className="fas fa-search fa-fw"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+
+
+					<nav className="mt-2">
+						<ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+							{
+								user.pagesUser.length !== 0 &&
+								user.pagesUser.map((page: any) => {
+									return (
+										<li className="nav-item">
+											<a href={`/${page.path}/`} className="nav-link">
+												<i className="nav-icon fas fa-tachometer-alt"></i>
+												<p>
+													{page.name}
+													<i className="right fas fa-angle-left"></i>
+												</p>
+												{
+													page.child &&
+													<ul className="nav nav-treeview">
+														{
+															page.child.map((childpage: any) => {
+																return (
+																	<li className="nav-item">
+																		<a href={`/${childpage.path}/`} className="nav-link">
+																			<i className="far fa-circle nav-icon"></i>
+																			<p>{childpage.name}</p>
+																		</a>
+																	</li>
+																)
+															})
+														}
+
+													</ul>
+												}
+											</a>
+										</li>
+									)
+								})
+							}
 							
-            </a>
-            <a href="/upload" className="nav-link">
-              <i className="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Обновление Айко
-                <i className="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <a href="/organization/" className="nav-link">
-              <i className="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Организации
-                <i className="right fas fa-angle-left"></i>
-              </p>
-							
-            </a>
-						<a href="/users/" className="nav-link">
-              <i className="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Пользователи
-                <i className="right fas fa-angle-left"></i>
-              </p>
-							
-            </a>
-						
-						
-						
-            
-          </li>
-					<li className="nav-item">
+							{
+								!user.role &&
+								ROUTE_PAGE.map((page: any) => {
+									return (
+										<li className="nav-item">
+											<a href={`/${page.path}/`} className="nav-link">
+												<i className="nav-icon fas fa-tachometer-alt"></i>
+												<p>
+													{page.name}
+													<i className="right fas fa-angle-left"></i>
+												</p>
+												{
+													page.child &&
+													<ul className="nav nav-treeview">
+														{
+															page.child.map((childpage: any) => {
+																return (
+																	<li className="nav-item">
+																		<a href={`/${childpage.path}/`} className="nav-link">
+																			<i className="far fa-circle nav-icon"></i>
+																			<p>{childpage.name}</p>
+																		</a>
+																	</li>
+																)
+															})
+														}
 
-					<a href="#" className="nav-link">
-              <i className="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Баннеры
-                <i className="right fas fa-angle-left"></i>
-              </p>
-							<ul className="nav nav-treeview">
-							<li className="nav-item">
-                <a href="/banners/mainbanner" className="nav-link">
-                  <i className="far fa-circle nav-icon"></i>
-                  <p>Баннеры</p>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="/banners/groops" className="nav-link">
-                  <i className="far fa-circle nav-icon"></i>
-                  <p>Группы</p>
-                </a>
-              </li>
-              
-              <li className="nav-item">
-                <a href="/banners/display" className="nav-link">
-                  <i className="far fa-circle nav-icon"></i>
-                  <p>Отображение</p>
-                </a>
-              </li>
-            </ul>
-            </a>
-					</li>
-					<li className="nav-item">
+													</ul>
+												}
+											</a>
+										</li>
+									)
+								})
+							}
 
-					<a href="/banners" className="nav-link">
-              <i className="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Заказы
-                <i className="right fas fa-angle-left"></i>
-              </p>
-							<ul className="nav nav-treeview">
-							<li className="nav-item">
-								<a href="/order/ordersDelivery" className="nav-link">
-                  <i className="far fa-circle nav-icon"></i>
-                  <p>Заказы доставка</p>
-                </a>
-                
-              </li>
-              
-            </ul>
-            </a>
-					</li>
-          
-        </ul>
-      </nav>
-      
-    </div>
-    
-  </aside>
-  )
+						</ul>
+					</nav>
+
+				</div>
+			}
+		</aside>
+	)
 }
 export default observer(Menu) 
