f36f9aca637197bf877265b22c8df46c2102c4ee
khinkalich <xirurgx3d@gmail.com>
2024-10-07 11:20:27 +0300

    Update auth.guard.ts

--- backend/src/guard/auth.guard.ts
+++ backend/src/guard/auth.guard.ts
@@ -1,10 +1,10 @@
 
 import {
 	BadRequestException,
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
+	CanActivate,
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
@@ -16,47 +16,47 @@ import { LoginServises } from 'src/components/common/auth/servises/login.servise
 export class AuthJWTGuard implements CanActivate {
 	constructor(
 		private jwtService: JwtService,
-		private userService:LoginServises
-		) {}
-	
-		async canActivate(context: ExecutionContext): Promise<boolean> {
-			const request = context.switchToHttp().getRequest();
-			const token = this.extractTokenFromHeader(request);
+		private userService: LoginServises
+	) { }
 
-			console.log('token',token);
-			if (!token) {
-				throw new UnauthorizedException();
-			}
-				
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest();
+		const token = this.extractTokenFromHeader(request);
 
-			/*
-      try {
-				const payload = await this.jwtService.verifyAsync(token.token, {
-					secret: process.env.SESSION_SECRET,
-				});
-				console.log('payload',payload);
-
-				if(!payload){
-					throw new UnauthorizedException();
-				}
-
-			} catch {
-				throw new UnauthorizedException();
-			}
-			*/
-			return true;
-    }
-
-		private extractTokenFromHeader(request: Request) {
-			let data = request.cookies["auth-cookie"];
-			if(!data){
-				return null;
-			}
-			return {
-				token:data.token,
-				refreshtoken:data.refreshToken
-			} 
+		//console.log('token',token);
+		if (!token) {
+			throw new UnauthorizedException();
 		}
 
-		
+
+		/*
+		try {
+			const payload = await this.jwtService.verifyAsync(token.token, {
+				secret: process.env.SESSION_SECRET,
+			});
+			console.log('payload',payload);
+
+			if(!payload){
+				throw new UnauthorizedException();
+			}
+
+		} catch {
+			throw new UnauthorizedException();
+		}
+		*/
+		return true;
+	}
+
+	private extractTokenFromHeader(request: Request) {
+		let data = request.cookies["auth-cookie"];
+		if (!data) {
+			return null;
+		}
+		return {
+			token: data.token,
+			refreshtoken: data.refreshToken
+		}
+	}
+
+
 }
