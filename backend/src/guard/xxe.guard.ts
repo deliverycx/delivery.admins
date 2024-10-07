fc68c6a5a60d8c726a136024dd2f5fdfbea7ccc0
khinkalich <xirurgx3d@gmail.com>
2024-10-07 16:24:55 +0300

    Update xxe.guard.ts

--- backend/src/guard/xxe.guard.ts
+++ backend/src/guard/xxe.guard.ts
@@ -4,13 +4,16 @@ import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
 export class XmlGuard implements CanActivate {
 	canActivate(context: ExecutionContext): boolean {
 		const request = context.switchToHttp().getRequest();
-		const xml = request.body._id;
+		if (request.body._id) {
+			const xml = request.body._id;
 
-		// Проверка на наличие DTD и внешних сущностей
+			// Проверка на наличие DTD и внешних сущностей
 
-		if (xml.includes('<!DOCTYPE') || xml.includes('select extractvalue')) {
-			return false; // блокируем запрос
+			if (xml.includes('<!DOCTYPE') || xml.includes('select extractvalue')) {
+				return false; // блокируем запрос
+			}
 		}
+
 
 		return true; // разрешаем запрос
 	}
