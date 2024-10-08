be76dcea58154c274fd09dfc4e55364ef2b2cb04
khinkalich <xirurgx3d@gmail.com>
2024-10-08 11:01:40 +0300

    Update base.repository.ts

--- backend/src/domain/repository/base.repository.ts
+++ backend/src/domain/repository/base.repository.ts
@@ -41,7 +41,7 @@ export class BaseRepository<ModelClass> {
 
 	async edit<T>(id: string, body: T) {
 		console.log('edit', id, body);
-		//await this.model.findByIdAndUpdate(id, body)
+		await this.model.findByIdAndUpdate(id, body)
 	}
 	async delete(id: string) {
 		await this.model.findOneAndDelete({
